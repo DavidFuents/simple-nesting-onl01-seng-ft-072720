4a292233c7065c05a87fbc4d29e040af8f4787a7
David Fuentes <davidf0827@gmail.com>
2020-08-03 06:55:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,6 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assemblu"]
+     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
 
 end
